539511b3dba025b7119379837c43759594973dcb
Stefankar1000 <stefankar1000@gmail.com>
2020-06-18 02:40:06 +0200

    Clean up the color scheme

--- config.h
+++ config.h
@@ -98,8 +98,8 @@ float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors (gruvbox dark) */
-	"#282828",
+	/* 8 normal colors */
+	"#1d2021",
 	"#cc241d",
 	"#98971a",
 	"#d79921",
@@ -108,7 +108,7 @@ static const char *colorname[] = {
 	"#689d6a",
 	"#a89984",
 
-	/* 8 bright colors (gruvbox dark) */
+	/* 8 bright colors */
 	"#928374",
 	"#fb4934",
 	"#b8bb26",
@@ -117,23 +117,16 @@ static const char *colorname[] = {
 	"#d3869b",
 	"#8ec07c",
 	"#ebdbb2",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"#1d2021",   /* background color (gruvbox:bg0_h) */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
